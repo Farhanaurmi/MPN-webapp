08e9609b7e1c71900f7b9620d98ea583537879da
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-03-26 12:54:58 +0600

    new image added at recipepost & option box added

--- MPN/recipepost/models.py
+++ MPN/recipepost/models.py
@@ -8,7 +8,7 @@ class Recipepost(models.Model):
     sno=models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     recipe = models.TextField()
-    photos = models.ImageField(default="p1.png",upload_to="images2/%y",null=True, blank=True)
+    photos = models.ImageField(default="p1.jpg",upload_to="images2/%y",null=True, blank=True)
     date_created = models.DateTimeField(default=now)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
 
