ab5ce775c1386f6f17881007202257bfee294d94
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-03-26 12:58:27 +0600

    fix 3.0

--- MPN/app1/models.py
+++ MPN/app1/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
 	user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
-	phone = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True, blank=True)
 	email = models.CharField(max_length=200, null=True)
 	profile_pic = models.ImageField(default="unnamed.png", null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
